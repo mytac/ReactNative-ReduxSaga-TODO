0d8acaf5ea490bfb831bc216d81daf0b90caf815
张君卓 <my_tac@126.com>
2017-10-25 16:03:08 +0800

    添加progressBar组件

--- newOffical/js/Panel/DashBoard/index.js
+++ newOffical/js/Panel/DashBoard/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View, StyleSheet, Image } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import PropTypes from 'prop-types';
 
 import LeftCol from './LeftCol';
 import RightCol from './RightCol';
@@ -24,7 +26,8 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function Form() {
+export default function Form(props) {
+  const { ratio } = props;
   return (
     <View style={styles.mainStyle}>
       <Image
@@ -35,8 +38,21 @@ export default function Form() {
           <View style={styles.subStyle}><LeftCol /></View>
           <View style={styles.subStyle}><RightCol /></View>
         </View>
-        <View style={{ height: 5, backgroundColor: 'pink' }} />
+        <LinearGradient
+          colors={['#295cce', '#14b7e6', '#dddae4']}
+          end={{ x: 1, y: 0 }}
+          locations={[0, ratio * 0.01, 0]}
+          style={{ height: 8, backgroundColor: 'pink' }}
+        />
       </Image>
     </View>
   );
 }
+
+Form.propTypes = {
+  ratio: PropTypes.number.isRequired,
+};
+
+Form.defaultProps = {
+  ratio: 0,
+};
