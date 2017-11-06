acc219c1b467a88b5418852027ce853e34fea114
张君卓 <my_tac@126.com>
2017-11-06 13:29:11 +0800

    在根组件中添加Provider

--- newOffical/index.js
+++ newOffical/index.js
@@ -1,6 +1,26 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-import App from './App';
+import AppReducer from './js/reducer';
+import AppWithNavigationState from './js/route/AppNavigator';
+
+class ReduxExampleApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.store = createStore(AppReducer);
+  }
 
 
-AppRegistry.registerComponent('newOffical', () => App);
+  render() {
+    return (
+      <Provider store={this.store}>
+        <AppWithNavigationState />
+      </Provider>
+    );
+  }
+}
+
+
+AppRegistry.registerComponent('newOffical', () => ReduxExampleApp);
