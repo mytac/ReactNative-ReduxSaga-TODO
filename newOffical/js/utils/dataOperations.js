4d109fa4be26f2834571225877b4d8f0b5a20c2b
张君卓 <my_tac@126.com>
2017-11-08 14:54:43 +0800

    添加操作AsyncStorage的一些公共函数

---
+++ newOffical/js/utils/dataOperations.js
@@ -0,0 +1,24 @@
+import { AsyncStorage } from 'react-native';
+
+/* eslint-disable no-console */
+
+// 存数据
+export const setData = async (key, val) => {
+  try {
+    await AsyncStorage.setItem(key, val);
+    console.log('saved');
+  } catch (err) {
+    console.log('err', err);
+  }
+  return false;
+};
+
+// 异步取数据
+export const getData = (key, handleData) => AsyncStorage.getItem(key)
+  .then(handleData)
+  .catch((err) => { console.log(err); });
+
+// 清空所有数据
+export const clearData = handleData => AsyncStorage.clear()
+  .then(handleData)
+  .catch(err => console.log(err));
