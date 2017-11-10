d01cf989d714b19f3f1deb870efd73fed80ce4c5
张君卓 <my_tac@126.com>
2017-11-10 11:29:57 +0800

    编写存入新值和获取所有数据的接口

--- newOffical/js/api.js
+++ newOffical/js/api.js
@@ -1,4 +1,5 @@
 import orderByDate from './utils/orderByDate';
+import { setData, getData, clearData } from './utils/dataOperations';
 
 const DATA = orderByDate([
   { time: '17:21', isDone: true, title: 'xxxx', description: 'bla bla bla', type: 'personal' },
@@ -12,3 +13,19 @@ const DATA = orderByDate([
 export default () => new Promise((resolve, reject) => {
   setTimeout(() => resolve(DATA), 1000);
 });
+
+// 存一个新值
+export function pushData(newData) {
+  return getData('data', (res) => {
+    const source = JSON.parse(res);
+    const copy = [].concat(source);
+    copy.push(newData);
+    setData('data', copy);
+    return copy;
+  });
+}
+
+// 获取所有数据
+export function fetchData() {
+  return getData('data', res => JSON.parse(res));
+}
