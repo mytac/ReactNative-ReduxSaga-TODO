6f67510eb422c6f3caff960cd864660d35cb5211
张君卓 <my_tac@126.com>
2017-11-16 16:10:51 +0800

    :construction: grid架构

--- newOffical/js/Personal/index.js
+++ newOffical/js/Personal/index.js
@@ -1,18 +1,66 @@
 import React from 'react';
 import {
-  Text, View, StyleSheet,
+  Text, View, StyleSheet, ScrollView, Dimensions,
 } from 'react-native';
 
 import PropTypes from 'prop-types';
 
-const styles = StyleSheet.create({
+import transferByDpi from '../utils/transferByDpi';
 
-});
+// 模拟渲染grid中的数组
+const tempGridArray = Array(25).fill(1);
 
-export default function Form() {
+
+const Gallery = () => (
+  <View>
+    <Text>galley</Text>
+  </View>
+);
+
+const Grid = () => {
+  const { width } = Dimensions.get('window');
   return (
-    <View>
-      <Text>Personal</Text>
+    <View style={[styles.grid, { width: width / 3, height: width / 3 }]}>
+      <View style={styles.gridInner}>
+        <Text>Grid</Text>
+      </View>
     </View>
   );
+};
+
+const GridWrapper = () => (
+  <View style={styles.gridWrapper}>
+    {tempGridArray.map(a => <Grid />)}
+  </View>
+);
+
+
+export default function FirstPage() {
+  return (
+    <ScrollView>
+      <Gallery />
+      <GridWrapper />
+    </ScrollView>
+  );
 }
+
+const styles = StyleSheet.create(transferByDpi({
+  gridWrapper: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    borderTopWidth: 3,
+    borderTopColor: '#ddd',
+  },
+  grid: {
+    backgroundColor: '#ddd',
+    paddingRight: 3,
+    paddingBottom: 3,
+  },
+  gridInner: {
+    backgroundColor: '#fff',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}));
