9603a15d15c2e2d6a8316089a22813d32a23ca6f
张君卓 <my_tac@126.com>
2017-11-20 17:47:59 +0800

    :construction:将上一次停止的时间存储，来保持暂存的时间节点

--- newOffical/js/Timer/index.js
+++ newOffical/js/Timer/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Text, View, TouchableOpacity,ScrollView
+  Text, View, TouchableOpacity, ScrollView,
 } from 'react-native';
 
 import PropTypes from 'prop-types';
@@ -66,6 +66,8 @@ export default class Timer extends React.Component {
   constructor(props) {
     super(props);
     this.interval = null;
+    this.initTime = 0; // 初始时间，即按start的时间
+    this.pauseTime = 0;// 累加时间，从暂停到start的时间
     this.state = {
       rBtnText: 'START',
       lBtnText: 'COUNT',
@@ -80,9 +82,10 @@ export default class Timer extends React.Component {
   }
 
   start() {
-    const { isStart, isReset } = this.state;
+    const { isStart, countedTime } = this.state;
     if (isStart) {
       // 暂停计时状态
+      this.pauseTime = countedTime; // 将上一次停止的时间存储，来保持暂存的时间节点
       this.setState({
         isStart: false,
         rBtnText: 'START',
@@ -91,12 +94,13 @@ export default class Timer extends React.Component {
       });
       clearInterval(this.interval);
       // 清空初始时间
-      this.initTime = null;
+      this.initTime = 0;
     } else {
       // 点击start时，获取初始时间
       if (!this.initTime) {
         this.initTime = (new Date()).getTime();
       }
+
       // 开始计时状态
       this.setState({
         isStart: true,
@@ -106,12 +110,13 @@ export default class Timer extends React.Component {
       this.interval = setInterval(
         () => {
           const currentTime = (new Date()).getTime();
-          const countingTime = currentTime - this.initTime;
+          const countingTime = (currentTime - this.initTime) + this.pauseTime;
           const minute = formatNum(countingTime / (60 * 1000));
           const senconds = formatNum((countingTime - (minute * 60000)) / 1000);
           const millSeconds = formatNum((countingTime % 1000) / 10);
           this.setState({
             totalTime: `${minute}:${senconds}:${millSeconds}`,
+            countedTime: countingTime,
           });
         },
         50,
@@ -122,10 +127,12 @@ export default class Timer extends React.Component {
     const { isStart, totalTime } = this.state;
     if (!isStart) {
       // 复位状态
-      this.initTime = null;
+      this.initTime = 0;
+      this.pauseTime = 0;
       this.setState({
         totalTime: '00:00:00',
         list: [],
+        countedTime: 0,
       });
     } else {
       // 计时状态
